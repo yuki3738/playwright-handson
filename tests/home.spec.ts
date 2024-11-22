9dca1ae34ec5a56fc7048c1035b95cdad709442e
yuki3738 <y.minamiya@gmail.com>
2024-11-22 12:20:52 +0900

    1.3.2まで

---
+++ tests/home.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('ページの表示テスト', async ({page}) => {
+  await page.goto('http://localhost:3001/');
+  await expect(page).toHaveTitle(/最初のページ/)
+  await expect(page.getByRole('heading')).toHaveText(/Playwrightハンズオン/)
+  await expect(page.getByText(/あなたは1週間後にはE２Eテストのエキスパートです。/)).toBeVisible()
+  await expect(page.getByRole('button', {name: /操作ボタン/})).toBeVisible()
+})
